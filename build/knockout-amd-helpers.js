7b6b7de5689e59f690b7ce7ad47f28e436c88193
Ryan Niemeyer <rniemeyer@gmail.com>
2013-11-01 16:09:13 -0500

    avoid dependencies on disposal check, even in dom disposal

--- build/knockout-amd-helpers.js
+++ build/knockout-amd-helpers.js
@@ -1,4 +1,4 @@
-// knockout-amd-helpers 0.5.1 | (c) 2013 Ryan Niemeyer |  http://www.opensource.org/licenses/mit-license
+// knockout-amd-helpers 0.5.2 | (c) 2013 Ryan Niemeyer |  http://www.opensource.org/licenses/mit-license
 define(["knockout"], function(ko) {
 
 //helper functions to support the binding and template engine (whole lib is wrapped in an IIFE)
@@ -75,15 +75,18 @@ ko.bindingHandlers.module = {
 
         //disposal function to use when a module is swapped or element is removed
         disposeModule = function() {
-            var currentData = templateBinding.data();
-            if (currentData) {
-                if (typeof currentData[disposeMethod] === "function") {
-                    currentData[disposeMethod].call(currentData);
-                    currentData = null;
-                }
+            //avoid any dependencies
+            ko.computed(function() {
+                var currentData = templateBinding.data();
+                if (currentData) {
+                    if (typeof currentData[disposeMethod] === "function") {
+                        currentData[disposeMethod].call(currentData);
+                        currentData = null;
+                    }
 
-                templateBinding.data(null);
-            }
+                    templateBinding.data(null);
+                }
+            }).dispose();
         };
 
         //now that we have bound our element using the template binding, pull the module and populate the observable.
@@ -107,7 +110,7 @@ ko.bindingHandlers.module = {
                 }
 
                 //if there is a current module and it has a dispose callback, execute it and clear the data
-                ko.computed(disposeModule).dispose();
+                disposeModule();
 
                 //at this point, if we have a module name, then require it dynamically
                 if (moduleName) {
--- src/amdBindings.js
+++ src/amdBindings.js
@@ -39,15 +39,18 @@ ko.bindingHandlers.module = {
 
         //disposal function to use when a module is swapped or element is removed
         disposeModule = function() {
-            var currentData = templateBinding.data();
-            if (currentData) {
-                if (typeof currentData[disposeMethod] === "function") {
-                    currentData[disposeMethod].call(currentData);
-                    currentData = null;
-                }
+            //avoid any dependencies
+            ko.computed(function() {
+                var currentData = templateBinding.data();
+                if (currentData) {
+                    if (typeof currentData[disposeMethod] === "function") {
+                        currentData[disposeMethod].call(currentData);
+                        currentData = null;
+                    }
 
-                templateBinding.data(null);
-            }
+                    templateBinding.data(null);
+                }
+            }).dispose();
         };
 
         //now that we have bound our element using the template binding, pull the module and populate the observable.
@@ -71,7 +74,7 @@ ko.bindingHandlers.module = {
                 }
 
                 //if there is a current module and it has a dispose callback, execute it and clear the data
-                ko.computed(disposeModule).dispose();
+                disposeModule();
 
                 //at this point, if we have a module name, then require it dynamically
                 if (moduleName) {
