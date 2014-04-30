4104a0d7b6d6efca88b6986ae1ac24c598bf9398
Tim Ambler <tkambler@gmail.com>
2014-04-30 08:24:06 -0500

    typo

--- examples/modules/fruits.js
+++ examples/modules/fruits.js
@@ -6,7 +6,7 @@ define([
 
         'template': "<div class='fruits large-12 columns'><p><ul data-bind='foreach: fruits'><li data-bind='html: $data'></li></ul></p></div>",
 
-        'fruits': ['Orange', 'Pineapple', 'Strawberry', 'Magno', 'Grape']
+        'fruits': ['Orange', 'Pineapple', 'Strawberry', 'Mango', 'Grape']
 
     };
 
